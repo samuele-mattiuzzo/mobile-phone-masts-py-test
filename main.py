fed464c44da616b964cde86ee0627e2e339b8c8a
Samuele Mattiuzzo <samumatt@gmail.com>
2019-11-23 17:33:51 +0000

    Polished main using new human readable printing functions from utils

--- main.py
+++ main.py
@@ -1,36 +1,39 @@
 # -*- coding: utf-8 -*-
 import tasks
-from utils import CSV_FILE_HEADERS_INDEX
+from utils import file_headers_to_human_readable, to_human_readable
 
 
 # Helpers to print to console
-def print_file_headers_to_list():
-    print(list(CSV_FILE_HEADERS_INDEX.keys()))
-
-
 def task_1_to_console():
     res = tasks.task_1()
-    print_file_headers_to_list()
-    print(res)
+
+    print(file_headers_to_human_readable())
+    for row in res:
+        print(to_human_readable(row))
 
 
 def task_2_to_console():
     res = tasks.task_2()
-    print_file_headers_to_list()
-    print(res[0])
-    print('Sum of all rents:', res[1])
+
+    print(file_headers_to_human_readable())
+    for row in res[0]:
+        print(to_human_readable(row))
+    print('Sum of all rents: {}'.format(res[1]))
 
 
 def task_3_to_console():
     res = tasks.task_3()
+
     for tenant, masts in res.items():
         print("{} rents {} masts".format(tenant, masts))
 
 
 def task_4_to_console():
     res = tasks.task_4()
-    print_file_headers_to_list()
-    print(res)
+
+    print(file_headers_to_human_readable())
+    for row in res:
+        print(to_human_readable(row))
 
 
 def run_all():
@@ -41,7 +44,7 @@ def run_all():
 
 
 if __name__ == '__main__':
-    user_choice = input('Insert a number [1,2,3,4] to run a specific task or leave empty to run them all.')
+    user_choice = input('Insert a number [1,2,3,4] to run a specific task or leave empty to run them all: ')
 
     if not user_choice:
         run_all()
